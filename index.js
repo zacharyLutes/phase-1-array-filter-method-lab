bb4c04e24b89fe2e415d3cb0c34796afd62d5853
forkingits1123 <lblack11235813@gmail.com>
2021-05-26 11:24:06 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const findMatching = (driverArray, string) => {
+    const match = driverArray.filter(person => person.toLowerCase() === string.toLowerCase());
+    return match;
+}
+
+const fuzzyMatch = (driverArray, string) => {
+    let sw = string[0];
+    const match = driverArray.filter(person => person.startsWith(sw));
+    return match;
+}
+
+const matchName = (driverArray, string) => {
+    const match = driverArray.filter(person => person.name === string);
+    return match;
+
+}
